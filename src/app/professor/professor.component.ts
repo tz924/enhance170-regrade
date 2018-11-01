cf2d73790d9b08e3730db24eea457c0731a2c3f6
Jing Zhang <jiz346@g.harvard.edu>
2018-10-31 18:46:04 -0700

    Basic functionalities working

--- src/app/professor/professor.component.ts
+++ src/app/professor/professor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Query } from '@angular/core';
 
 @Component({
   selector: 'app-professor',
@@ -7,18 +7,128 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessorComponent implements OnInit {
 
+  questions: Question[];
+  checked: Question[];
+  deleted: Question[];
+  lecture: Lecture;
+  showQuestions: boolean;
+  showChecked: boolean;
+  showDeleted: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    // Initialize necessary objects
+    this.questions = [];
+    this.checked = [];
+    this.deleted = [];
+    this.showQuestions = true;
+    this.showChecked = false;
+    this.showDeleted = false;
+
+    this.lecture = {
+      title: 'Lecture 1',
+      time: new Date(2018, 10, 24, 10, 33, 30, 0),
+      nbrOnline: 121
+    };
+
+    // Make up some fake datas
+    this.questions.push({
+      index: 1,
+      content: 'Compliment interested discretion estimating on stimulated \
+      apartments. Dear so sing when in find read of call. As distrusts \
+      behaviour abilities defective is. Never at water me might.',
+      duration: 13,
+      nbrAnswers: 12,
+      nbrLikes: 10,
+    });
+
+    this.questions.push({
+      index: 2,
+      content: 'Compliment interested discretion estimating on stimulated \
+      apartments oh. Dear so sing when in find read of call. As distrusts \
+      behaviour abilities defective is. Never at water me might.',
+      duration: 4,
+      nbrAnswers: 1,
+      nbrLikes: 10,
+    });
+
+    this.questions.push({
+      index: 3,
+      content: 'Compliment interested discretion estimating on stimulated \
+      apartments oh. Dear so sing when in find read of call. As distrusts \
+      behaviour abilities defective is. Never at water me might.',
+      duration: 1,
+      nbrAnswers: 1,
+      nbrLikes: 10,
+    });
+
+    this.questions.push({
+      index: 11,
+      content: 'Compliment interested discretion estimating on stimulated \
+      apartments oh. Dear so sing when in find read of call. As distrusts \
+      behaviour abilities defective is. Never at water me might.',
+      duration: 1,
+      nbrAnswers: 1,
+      nbrLikes: 10,
+    });
+
+  }
+
+  checkQuestion(index: number) {
+    let question: Question;
+    for (let i = 0; i < this.questions.length; i++) {
+      if (this.questions[i].index === index) {
+        question = this.questions.splice(i, 1)[0];
+      }
+    }
+
+    this.checked.push(question);
+  }
+
+  deleteQuestion(index: number) {
+    let question: Question;
+    for (let i = 0; i < this.questions.length; i++) {
+      if (this.questions[i].index === index) {
+        question = this.questions.splice(i, 1)[0];
+      }
+    }
+
+    this.deleted.push(question);
+  }
+
+  showNormalQuestions() {
+    this.showQuestions = true;
+    this.showChecked = false;
+    this.showDeleted = false;
+  }
+
+  showCheckedQuestions() {
+    this.showChecked = true;
+    this.showDeleted = false;
+    this.showQuestions = false;
+  }
+
+  showDeletedQuestions() {
+    this.showDeleted = true;
+    this.showChecked = false;
+    this.showQuestions = false;
   }
 
 }
 
+
+
 interface Question {
+  index: number;
   content: string;
   duration: number;
-  replies: number;
-  likes: number;
-  index: number;
+  nbrAnswers: number;
+  nbrLikes: number;
+}
 
+interface Lecture {
+  title: string;
+  time: Date;
+  nbrOnline: number;
 }
